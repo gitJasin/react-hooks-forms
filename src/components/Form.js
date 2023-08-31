6f158507363bf5f3c239cb14cafb09c4b87d1204
Jason Stewart <jaysin.stewart@gmail.com>
2023-08-31 12:33:40 -0700

    completed form lab

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form({
+  firstName,
+  lastName,
+  onFirstNameChange,
+  onLastNameChange
+}) {
+
+  const [number, setNumber] = useState(0)
+  const [isInvalidNumber, setIsInvalidNumber] = useState(null)
+
+  function handleNumberChange(event) {
+    const newNumber = parseInt(event.target.value)
+    if (newNumber >= 0 && newNumber <= 5) {
+      setNumber(newNumber)
+      setIsInvalidNumber(null)
+    } else {
+      setIsInvalidNumber(`${newNumber} is not a valid number! Must bebetween 0-5`)
+    }
+  }
 
   return (
-    <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form>
+        <input type="text" onChange={onFirstNameChange} value={firstName} />
+        <input type="text" onChange={onLastNameChange} value={lastName} />
+        <input type="number" value={number} onChange={handleNumberChange} />
+        {isInvalidNumber ? <span style={{ color: "red" }}>{isInvalidNumber}</span> : null}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
